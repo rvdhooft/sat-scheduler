154a1152cd1efe223cf69bd011cd459108d99756
Ruth Vanderhooft <rvanderhooft@ostusa.com>
2023-03-03 12:54:57 -0500

    Added logrocket

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,12 @@ import { ParamProvider } from './contexts/paramContext';
 import { StudentProvider } from './contexts/studentContext';
 import Fallback from './Fallback';
 
+if (import.meta.env.VITE_APP_LOGROCKET_KEY) {
+  import('logrocket').then(({ default: LogRocket }) => {
+    LogRocket.init(import.meta.env.VITE_APP_LOGROCKET_KEY);
+  });
+}
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ErrorBoundary FallbackComponent={Fallback}>
